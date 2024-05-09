da463d8dd05a1a5ec11633733d0075b970fad666
Benjamin2404-AB <benab2404@gmail.com>
2024-05-09 21:18:03 +0100

    adjust weight

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Components/Navbar";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import "@fontsource/poppins"; 
 import "@fontsource/poppins/400.css";
-import "@fontsource/poppins/900.css";
+import "@fontsource/poppins/800.css";
 
 
 function App() {
